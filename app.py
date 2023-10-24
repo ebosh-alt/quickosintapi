93003d1871ca3b91103b7cc9aa6a295b0cb7747b
Nikita I <isakovn2005@gmail.com>
2023-10-24 13:01:18 +0300

    add authentication

--- app.py
+++ app.py
@@ -1,13 +1,50 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
 
-from config import token
+from config import token, path, name_login, password_login
 from workApi import Quick
 
 app = Flask(__name__)
+app.secret_key = 'qwdvioknghe4352ncuIK*asde'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
-@app.route('/quickosintapiJkB6kG7RTMEsZ98T92*jyeUoSEC')
-def hello_world():
+class User(UserMixin):
+    pass
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    user = User()
+    user.id = user_id
+    return user
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        name = request.form['name']
+        password = request.form['password']
+        if name == name_login and password == password_login:
+            user = User()
+            user.id = 1
+            login_user(user)
+            return redirect(url_for("api"))
+    return render_template('login.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+@app.route(f'/{path}')
+@login_required
+def api():
     find = request.args.get("find")
     choice = request.args.get("choice")
     quick = Quick(token)
